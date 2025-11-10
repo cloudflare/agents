02f60844c4001e925a3173c17130810b07fdfef7
Ishaan Gupta <ishaangupta999@gmail.com>
2025-11-11 02:24:06 +0530

    fixed basic config, easier to install by user npw

--- examples/tictactoe/vite.config.ts
+++ examples/tictactoe/vite.config.ts
@@ -1,7 +1,14 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
-import react from "@vitejs/plugin-react";
+import path from "node:path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), cloudflare()]
+  plugins: [cloudflare(), react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 });
